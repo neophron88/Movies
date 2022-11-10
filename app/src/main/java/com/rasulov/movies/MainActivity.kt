dceef6ce6ab5148a436ef06a83dfe16db8559431
it0088 <itrf0088@gmail.com>
2022-11-10 16:40:23 +0500

    Gradle configure and project modularization

---
+++ app/src/main/java/com/rasulov/movies/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rasulov.movies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rasulov.movies.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
